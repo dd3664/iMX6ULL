58b037537536d737f5f669ddaaae15fac7723139
dongdong <scudongdong@163.com>
2023-05-17 08:59:59 -0700

    修改openssl编译脚本,更改安装文件夹名称为install

--- opensource/openssl-1.1.1t/build.sh
+++ opensource/openssl-1.1.1t/build.sh
@@ -1,5 +1,6 @@
-mkdir -p ../libopenssl
-./config shared no-asm --prefix=/home/dong//Project/iMX6ULL/opensource/openssl-1.1.1t/libopenssl/ CROSS_COMPILE=/usr/local/arm/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+mkdir -p $(pwd)/install
+./config shared no-asm --prefix=$(pwd)/install CROSS_COMPILE=/usr/local/arm/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
 #配置成功后生成Makefile，手动删除Makefile中"-m64"
 make -j4 V=s
 make install
+
