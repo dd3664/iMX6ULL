19209c3e8bc9e9b1251b2b1428fa5527454f5d33
dongdong <scudongdong@163.com>
2023-05-19 08:27:04 -0700

    删除环境有问题的libnl

--- opensource/libnl-3.2.25/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-mkdir -p $(pwd)/libnl
-
-./configure CC=/usr/local/arm/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc \
-    --prefix=$(pwd)/libnl \
-    --host=arm-linux-gnueabihf \
-    CFLAGS=-I/usr/include/libnl3
-
-    make -j4 V=s  && make install
