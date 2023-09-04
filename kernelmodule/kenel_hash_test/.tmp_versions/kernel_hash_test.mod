bfcfa5a25be4bb828f47ffd3537d2e4744a13bbd
dongdong <scudongdong@163.com>
2023-09-04 09:53:30 -0700

    内核哈希表使用示例

---
+++ kernelmodule/kenel_hash_test/.tmp_versions/kernel_hash_test.mod
@@ -0,0 +1,2 @@
+/home/dong/Project/iMX6ULL/kernelmodule/kenel_hash_test/kernel_hash_test.ko
+/home/dong/Project/iMX6ULL/kernelmodule/kenel_hash_test/kernel_hash_test.o
---
+++ kernelmodule/kenel_hash_test/kernel_hash_test.c
@@ -0,0 +1,61 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/hashtable.h>
+#include <linux/slab.h>
+
+typedef struct student {
+    int id;
+    char name[32];
+    struct hlist_node hnode; /* 哈希表节点 */
+} HASHTEST;
+
+DEFINE_HASHTABLE(student_table, 8); /* 定义哈希表，8表示哈希表的大小 */
+
+static int __init hash_test_init(void)
+{
+    HASHTEST *s;
+    HASHTEST *found;
+    int id_to_find = 42;
+
+    //创建学生记录
+    s = kmalloc(sizeof(HASHTEST), GFP_KERNEL);
+    if (!s)
+    {
+        printk(KERN_ERR "Failed to allocate memory for student\n");
+        return -ENOMEM;
+    }
+    s->id = 42;
+    strcpy(s->name, "dongdong");
+
+    //添加学生记录到哈希表
+    hash_add(student_table, &s->hnode, s->id);
+
+    //查找学生记录
+    hash_for_each_possible(student_table, found, hnode, id_to_find)
+    {
+        if (found->id == id_to_find)
+        {
+            printk(KERN_INFO "Student found: ID= %d, Name=%s\n", found->id, found->name);
+            break;
+        }
+    }
+    return 0;
+}
+
+static void __exit hash_test_exit(void)
+{
+    HASHTEST *s;
+    HASHTEST *tmp;
+    int bkt;
+    //遍历哈希表并释放内存
+    hash_for_each_safe(student_table, bkt, s, tmp, hnode)
+    {
+        hash_del(&s->hnode);
+        kfree(s);
+    }
+}
+
+module_init(hash_test_init);
+module_exit(hash_test_exit);
+
+MODULE_LICENSE("GPL");
