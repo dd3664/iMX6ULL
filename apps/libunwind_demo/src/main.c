fdc1c67e2f26f6dd740eb400d83118448a800830
dongdong <scudongdong@163.com>
2024-08-05 23:14:30 +0800

    调整libunwind_demo：新增返回值判断；调整编译方式为静态链接，避免拷贝动态链接库至开发板

--- apps/libunwind_demo/src/main.c
+++ apps/libunwind_demo/src/main.c
@@ -1,6 +1,7 @@
 /****************************************************************************************************/
 /*                                           INCLUDE                                                */
 /****************************************************************************************************/
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -28,19 +29,46 @@ void print_stacktrace() {
     unw_word_t ip, sp;
     char func_name[256];
     unw_word_t offset;
+	int ret;
 
     // Initialize the cursor to current frame for local unwinding
-    unw_getcontext(&context);
-    unw_init_local(&cursor, &context);
+    if (0 != unw_getcontext(&context))
+	{
+		printf("unw_getcontext failed\n");
+		return;
+	}
+    if (0 != unw_init_local(&cursor, &context))
+	{
+		printf("unw_init_local failed\n");
+		return;
+	}
 
     printf("Call stack:\n");
-    while (unw_step(&cursor) > 0) {
-        unw_get_reg(&cursor, UNW_REG_IP, &ip);
-        unw_get_reg(&cursor, UNW_REG_SP, &sp);
-        unw_get_proc_name(&cursor, func_name, sizeof(func_name), &offset);
-
+    do
+	{
+        if (0 != unw_get_reg(&cursor, UNW_REG_IP, &ip))
+		{
+			printf("unw_get_reg failed\n");
+			return;
+		}
+        if (0 != unw_get_reg(&cursor, UNW_REG_SP, &sp))
+		{
+			printf("unw_get_reg failed\n");
+			return;
+		}
+        if (0 != unw_get_proc_name(&cursor, func_name, sizeof(func_name), &offset))
+		{
+			snprintf(func_name, sizeof(func_name), "%s", "??????");
+			offset = 0;
+		}
         printf("ip = 0x%lx, sp = 0x%lx, function = %s+0x%lx\n", (long)ip, (long)sp, func_name, (long)offset);
-    }
+
+		ret = unw_step(&cursor);
+		if (ret < 0)
+		{
+			printf("unw_step failed\n");
+		}
+    } while (ret > 0);
 }
 
 void foo() {
@@ -53,9 +81,5 @@ void bar() {
 
 int main() {
     bar();
-	while(1)
-	{
-		sleep(1);
-	}
     return 0;
 }
