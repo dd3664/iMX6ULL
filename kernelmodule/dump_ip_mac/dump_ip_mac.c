f21413e7531c14f28a0e4016388f71fa0f5d8146
dongdong <scudongdong@163.com>
2024-11-28 23:19:23 +0800

    调整内核打印skb ip地址和mac地址的方法，增加打印开关和打印频率的参数

--- kernelmodule/dump_ip_mac/dump_ip_mac.c
+++ kernelmodule/dump_ip_mac/dump_ip_mac.c
@@ -18,75 +18,175 @@
 /*                                           VARIABLES                                              */
 /****************************************************************************************************/
 static struct nf_hook_ops hook_ops; // Netfilter hook options structure
+int g_dbg_cnt = 0;
+int g_dbg_switch = 0;
+int g_dbg_cnt1 = 0;
+int g_dbg_switch1 = 0;
 /****************************************************************************************************/
 /*                                       STATIC FUNCTIONS                                           */
 /****************************************************************************************************/
-static void print_ip_mac(struct sk_buff *skb, unsigned int match_src_ip, unsigned int match_dest_ip, unsigned char *match_src_mac, unsigned char *match_dest_mac, char *call_func, int call_line)
+static void match_skb_by_ip(struct sk_buff *skb, const unsigned int match_src_ip, const unsigned int match_dest_ip, int *dbg_cnt, int dbg_freq, int *dbg_switch)
 {
-	struct iphdr *ip_header;
-    struct ethhdr *eth_header;
-    unsigned char *src_mac;
-    unsigned char *dest_mac;
-    unsigned int src_ip;
+	struct iphdr *iph;
+	unsigned int src_ip;
 	unsigned int dest_ip;
-
-	// Check if the skb is valid
-    if (!skb)
+	
+	if (!skb)
 	{
-        return;
+		goto out;
 	}
 
-    // Extract Ethernet header
-    eth_header = eth_hdr(skb);
-    if (!eth_header)
+	iph = ip_hdr(skb);
+	if (!iph)
 	{
-        return;
+		goto out;
 	}
 
-    // Extract source and destination MAC addresses
-    src_mac = eth_header->h_source;
-    dest_mac = eth_header->h_dest;
-
-    // Extract IP header
-    ip_header = ip_hdr(skb);
-    if (!ip_header)
-	{
-        return;
-	}
-
-    // Get source IP address
-    src_ip = ip_header->saddr;
-	dest_ip = ip_header->daddr;
+	src_ip = iph->saddr;
+	dest_ip = iph->daddr;
 
 	if ((0 != match_src_ip) && (src_ip != match_src_ip))
 	{
-		return;
+		goto out;
 	}
 
 	if ((0 != match_dest_ip) && (dest_ip != match_dest_ip))
 	{
-		return;
+		goto out;
+	}
+	
+	if (*dbg_cnt < dbg_freq - 1)
+	{
+		(*dbg_cnt)++;
+		goto out;
+	}
+	else
+	{
+		*dbg_cnt = 0;
 	}
 
-	if ((NULL != match_src_mac) && (0 != memcmp(src_mac, match_src_mac, 6)))
+	*dbg_switch = 1;
+	return;
+
+out:
+	*dbg_switch = 0;
+	return;
+}
+
+static void match_skb_by_mac(struct sk_buff *skb, unsigned char *match_src_mac, unsigned match_dest_mac, int *dbg_cnt, int dbg_freq, int *dbg_switch)
+{
+	struct ethhdr *eth_header;
+	unsigned char *src_mac;
+    unsigned char *dest_mac;
+
+	if (!skb)
+	{
+        goto out;
+	}
+
+    eth_header = eth_hdr(skb);
+    if (!eth_header)
+	{
+        goto out;
+	}
+
+    src_mac = eth_header->h_source;
+    dest_mac = eth_header->h_dest;
+
+	if ((NULL != match_src_mac) && (0 != memcmp(src_mac, match_src_mac, ETH_ALEN)))
+	{
+		goto out;
+	}
+
+	if ((NULL != match_dest_mac) && (0 != memcmp(dest_mac, match_dest_mac, ETH_ALEN)))
+	{
+		goto out;
+	}
+
+	if (*dbg_cnt < dbg_freq - 1)
+	{
+		(*dbg_cnt)++;
+		goto out;
+	}
+	else
+	{
+		*dbg_cnt = 0;
+	}
+
+	*dbg_switch = 1;
+	return;
+
+out:
+	*dbg_switch = 0;
+	return;
+}
+
+static void dump_ip(struct sk_buff *skb, int dbg_switch, const char *call_func, const int call_line)
+{
+	struct iphdr *iph;
+	unsigned int src_ip;
+	unsigned int dest_ip;
+
+	if (!dbg_switch)
+	{
+		return;
+	}
+	
+	if (!skb)
 	{
 		return;
 	}
 
-	if ((NULL != match_dest_mac) && (0 != memcmp(dest_mac, match_dest_mac, 6)))
+	iph = ip_hdr(skb);
+	if (!iph)
 	{
 		return;
 	}
 
-    printk("[%s:%d] Packet received: Source IP: %pI4, Destination IP: %pI4, Source MAC: %pM, Destination MAC: %pM\n", call_func, call_line, &src_ip, &dest_ip, src_mac, dest_mac);
+	src_ip = iph->saddr;
+	dest_ip = iph->daddr;
+
+	printk("[%s:%d] Source IP:%pI4, Destination IP:%pI4, skb->dev:%s\n", call_func, call_line, &src_ip, &dest_ip, skb->dev->name);
+	return;
+}
+
+static void dump_mac(struct sk_buff *skb, int dbg_switch, const char *call_func, const int call_line)
+{
+	struct ethhdr *eth_header;
+	unsigned char *src_mac;
+    unsigned char *dest_mac;
+
+	if (!dbg_switch)
+	{
+		return;
+	}
+
+	if (!skb)
+	{
+		return;
+	}
+
+	eth_header = eth_hdr(skb);
+	if (!eth_header)
+	{
+		return;
+	}
+
+	src_mac = eth_header->h_source;
+    dest_mac = eth_header->h_dest;
+
+	printk("[%s:%d] Source Mac:%pM, Destination Mac:%pM, skb->dev:%s\n", call_func, call_line, src_mac, dest_mac, skb->dev->name);
 	return;
 }
 
 static unsigned int hook_func(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
 {
-    unsigned int match_src_ip = 0x6400a8c0; /* 192.168.0.100的十六进制 */
-	unsigned char match_src_mac[6] = {0x8c, 0x32, 0x23, 0x1a, 0xe7, 0xe0};
-	print_ip_mac(skb, match_src_ip, 0, match_src_mac, NULL, __func__, __LINE__);
+	unsigned char match_src_mac[ETH_ALEN] = {0x8c, 0x32, 0x23, 0x1a, 0xe7, 0xe0};
+	unsigned char match_dest_mac[ETH_ALEN] = {0x08, 0x90, 0x90, 0x90, 0x90, 0x90};
+	match_skb_by_ip(skb, 0x6400a8c0, 0xc800a8c0, &g_dbg_cnt, 1, &g_dbg_switch); /* 0x6400a8c0: 192.168.0.100的十六进制，网络字节序 */
+	match_skb_by_mac(skb, match_src_mac, match_dest_mac, &g_dbg_cnt1, 1, &g_dbg_switch1);
+	dump_ip(skb, g_dbg_switch, __func__, __LINE__);
+	dump_mac(skb, g_dbg_switch1, __func__, __LINE__);
     return NF_ACCEPT;
 }
 
